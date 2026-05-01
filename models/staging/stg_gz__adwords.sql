11f55f959f88038272f1098590a3863acfab306a
ceren kocabaş <ceren.kocabas@hotmail.com>
2026-05-01 10:52:16 +0000

    refactor: move staging models to staging folder

--- models/staging/stg_gz__adwords.sql
+++ models/staging/stg_gz__adwords.sql
@@ -7,11 +7,10 @@ renamed as (
       date_date,
       paid_source,
       campaign_key,
-      impression,
-      click,
-        
         REPLACE(camPGN_name,  'é' , 'e') AS campaign_name,
-        CAST(ads_cost AS FLOAT64 ) AS ads_cost
+        CAST(ads_cost AS FLOAT64 ) AS ads_cost,
+        CAST(impression AS INT64 ) AS impression,
+        CAST(click AS INT64 ) AS click
     from source
 )
 
