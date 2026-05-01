b80545d673d1f5cf4068a4fbd2934c5483d47dc6
ceren kocabaş <ceren.kocabas@hotmail.com>
2026-05-01 10:37:20 +0000

    feat: add staging model for criteo campaigns

---
+++ models/stg_gz__criteo.sql
@@ -0,0 +1,17 @@
+with source as (
+    select * from {{ source('raw', 'criteo') }}
+),
+
+renamed as (
+    select
+      date_date,
+      paid_source,
+      campaign_key,
+      camPGN_name,
+        CAST(ads_cost AS FLOAT64 ) AS ads_cost,
+        CAST(impression AS INT64 ) AS impression,
+        CAST(click AS INT64 ) AS click
+    from source
+)
+
+select * from renamed
